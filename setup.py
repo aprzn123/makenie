fe50521b0b0726f5c53e9ab884eb75496d7e030f
aprzn123 <38828335+aprzn123@users.noreply.github.com>
2019-01-17 20:19:37 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='makenie',
+    version='1.0',
+    description='Creates NotImplementedErrors',
+    url='https://github.com/aprzn123/makenie',
+    author='aprzn123',
+    license='MIT',
+    packages=['funniest'],
+    zip_safe=False)
